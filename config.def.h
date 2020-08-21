5763d7cea039c3f42281e4523749fbbcd77dcbf6
Akshay Nair <phenax5@gmail.com>
2020-08-21 17:55:50 +0530

    Border size

--- config.def.h
+++ config.def.h
@@ -22,7 +22,7 @@ static const Layout layouts[] = {
 
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const int gappx              = 4;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
