6f139b149729d6bdf7316ec620fe5a2e74124e16
Akshay Nair <phenax5@gmail.com>
2023-10-01 17:21:26 +0530

    fix: mouse float move binding

--- config.def.h
+++ config.def.h
@@ -150,19 +150,20 @@ static const Key keys[] = {
 /* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static const Button buttons[] = {
-	/* click                event mask      button          function        argument */
-	// { ClkWinTitle,          0,              Button2,        zoom,           {0} },
-	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
-	// { ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
-	{ ClkClientWin,         MODKEY|ALTKEY,  Button1,        resizemouse,    {0} },
-	{ ClkTagBar,            0,              Button1,        view,           {0} },
-	// { ClkTagBar,            0,              Button3,        toggleview,     {0} },
-	// { ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
-	// { ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+	/* click                event mask            button          function        argument */
+	{ ClkClientWin,         MODKEY|ControlMask,   Button1,        movemouse,      {0} },
+	{ ClkClientWin,         MODKEY|ALTKEY,        Button1,        resizemouse,    {0} },
+	{ ClkTagBar,            0,                    Button1,        view,           {0} },
+	// { ClkWinTitle,          0,                    Button2,        zoom,           {0} },
+	// { ClkClientWin,         MODKEY,               Button2,        togglefloating, {0} },
+	// { ClkTagBar,            0,                    Button3,        toggleview,     {0} },
+	// { ClkTagBar,            MODKEY,               Button1,        tag,            {0} },
+	// { ClkTagBar,            MODKEY,               Button3,        toggletag,      {0} },
 };
 
 static const char* autostart_cmds[] = {
 	"~/nixos/packages/dwm/autostart.sh &",
+	"xset r rate 350 30",
 	// "~/scripts/monitor.sh on &",
 };
 
